b7caeaa99ffdcd62d140ce2ccd50904094597869
John Harris <john.harris@nexusmods.com>
2023-02-02 14:10:53 +0000

    fix: log_rescued_responses not available in rails 6

--- lib/nexus_semantic_logger/extensions/action_dispatch/debug_exceptions.rb
+++ lib/nexus_semantic_logger/extensions/action_dispatch/debug_exceptions.rb
@@ -11,7 +11,8 @@ module ActionDispatch
     undef_method :log_error
 
     def log_error(request, wrapper)
-      return if !log_rescued_responses?(request) && wrapper.rescue_response?
+      # log_rescued_responses? is a rails7 feature, but this gem is also used on rails6. Check for its existence.
+      return if respond_to?('log_rescued_responses?') && !log_rescued_responses?(request) && wrapper.rescue_response?
 
       ActiveSupport::Deprecation.silence do
         ActionController::Base.logger.fatal(wrapper.exception)
