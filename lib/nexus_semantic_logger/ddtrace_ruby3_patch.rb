bf5725accce5dd88d7a6bc83924ab65f0f28027e
John Harris <john.harris@nexusmods.com>
2023-03-01 13:17:07 +0000

    style: cq

--- lib/nexus_semantic_logger/ddtrace_ruby3_patch.rb
+++ lib/nexus_semantic_logger/ddtrace_ruby3_patch.rb
@@ -9,7 +9,8 @@ Datadog::Core::Environment::VMCache.class_eval do
   module_function
 
   # Ruby >= 3.2 uses :constant_cache_invalidations instead of :global_constant_state
-  # This is a temporary workaround, the correct solution is to report both :constant_cache_invalidations and :constant_cache_misses in DD
+  # This is a temporary workaround, the correct solution is to report both :constant_cache_invalidations and
+  # :constant_cache_misses in DD
   # See https://github.com/ruby/ruby/pull/5433,
   #     https://github.com/DataDog/dd-trace-rb/blob/v1.5.0/lib/datadog/core/environment/vm_cache.rb#L23-L25
   def global_constant_state
