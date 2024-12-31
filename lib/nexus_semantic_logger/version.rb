24c6913f5ba5a3eb8363249d1e93bb5488eec053
Tobias Glazebrook <tobias.glazebrook@nexusmods.com>
2024-12-31 11:26:56 +0000

    fix: specify version number for local testing

--- lib/nexus_semantic_logger/version.rb
+++ lib/nexus_semantic_logger/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 module NexusSemanticLogger
   # Leave this as 0.0.0 in order for CI process to replace with the tagged version.
-  VERSION = '0.0.0'
+  VERSION = '1.13.6'
 end
